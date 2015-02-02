5205dc5dcfa0cef95c4d0e42cfe02880c9eb3b0e
Peter Chen <peter_chen@trimble.com>
2015-02-02 15:27:14 +0800

    Initial Version

---
+++ WalkingGame.Android/GameActivity.cs
@@ -0,0 +1,26 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace WalkingGame.Droid
+{
+    [Activity(Label = "Walking"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , AlwaysRetainTaskState = true
+        , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
+        , ScreenOrientation = ScreenOrientation.SensorLandscape
+        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden)]
+    public class GameActivity : Microsoft.Xna.Framework.AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Game1.Activity = this;
+            var g = new Game1();
+            SetContentView(g.Window);
+            g.Run();
+        }
+    }
+}
+
